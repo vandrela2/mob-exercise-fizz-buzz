6fec30ace0f70b98a62b91dd191ed6d8596f37b7
vandrela2 <vasyl.andrela@and.digital>
2023-07-18 11:09:02 +0100

    test for first excersice

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,11 +1,17 @@
 const prod_code = (number) => {
-
+if(number % 3 !== 0)
 };
 
-describe('Fizz Buzz', () => {
-    it('', () => {
-        // Arrange
-        // Act
-        // Assert
-    });
+describe("Fizz Buzz", () => {
+  it("if fizz is returned if the number is divisible by 3", () => {
+    // Arrange
+    // Act
+    // Assert
+    expect(prod_code(9)).toBe('Fizz');
+  });
+  it("", () => {
+    // Arrange
+    // Act
+    // Assert
+  });
 });
