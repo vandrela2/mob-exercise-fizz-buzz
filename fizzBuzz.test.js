7f16b3e0bdb8794a43e5b60efadfa64b3e275614
vandrela2 <vasyl.andrela@and.digital>
2023-07-18 13:55:21 +0100

    almost done

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -10,29 +10,28 @@ const prod_code = (number) => {
   }
 };
 
+const loopCode = (number) => {
+  const arrayResult = [];
+  for (i = 1; i <= number; i++) {
+    arrayResult.push(prod_code(i));
+  }
+  return arrayResult;
+};
+
 describe("Fizz Buzz", () => {
   it("if fizz is returned if the number is divisible by 3", () => {
-    // Arrange
-    // Act
-    // Assert
     expect(prod_code(9)).toBe("Fizz");
   });
   it("if buzz is returned if the number is divisable by 5", () => {
-    // Arrange
-    // Act
-    // Assert
     expect(prod_code(10)).toBe("Buzz");
   });
   it("if FizzBuzz is returned if the number is divisable by 3 and 5", () => {
-    // Arrange
-    // Act
-    // Assert
     expect(prod_code(15)).toBe("FizzBuzz");
   });
   it("Returns number if it is not divisible by 3 or 5", () => {
-    // Arrange
-    // Act
-    // Assert
     expect(prod_code(7)).toBe(7);
+  });
+  it("FizzBuzz loop test", () => {
+    expect(loopCode(5)).toEqual([1, 2, "Fizz", 4, "Buzz"]);
   });
 });
