85aa91eb27c35b37f258b7b339e5898fe1cfb2f8
vandrela2 <vasyl.andrela@and.digital>
2023-07-18 12:00:02 +0100

    finished first ex

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,10 +1,12 @@
 const prod_code = (number) => {
-  if (number % 3 == 0) {
-    return "Fizz";
+  if (number % 3 == 0 && number % 5 == 0) {
+    return "FizzBuzz";
   } else if (number % 5 == 0) {
     return "Buzz";
-  } else if (number % 3 == 0 && number % 5 == 0) {
-    return "FizzBuzz";
+  } else if (number % 3 == 0) {
+    return "Fizz";
+  } else {
+    return number;
   }
 };
 
@@ -26,5 +28,11 @@ describe("Fizz Buzz", () => {
     // Act
     // Assert
     expect(prod_code(15)).toBe("FizzBuzz");
+  });
+  it("Returns number if it is not divisible by 3 or 5", () => {
+    // Arrange
+    // Act
+    // Assert
+    expect(prod_code(7)).toBe(7);
   });
 });
