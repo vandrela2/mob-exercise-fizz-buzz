c919749c9411f35add8a8a202e2af0346938dd2b
Tyrone Bramwell <Tyrone.Bramwell@And.Digital>
2023-07-18 11:37:04 +0100

    added tests for buzz and fizz

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,5 +1,11 @@
 const prod_code = (number) => {
-if(number % 3 !== 0)
+  if (number % 3 == 0) {
+    return "Fizz";
+  } else if (number % 5 == 0) {
+    return "Buzz";
+  } else if (number % 3 == 0 && number % 5 == 0) {
+    return "FizzBuzz";
+  }
 };
 
 describe("Fizz Buzz", () => {
@@ -7,11 +13,18 @@ describe("Fizz Buzz", () => {
     // Arrange
     // Act
     // Assert
-    expect(prod_code(9)).toBe('Fizz');
+    expect(prod_code(9)).toBe("Fizz");
   });
-  it("", () => {
+  it("if buzz is returned if the number is divisable by 5", () => {
     // Arrange
     // Act
     // Assert
+    expect(prod_code(10)).toBe("Buzz");
+  });
+  it("if FizzBuzz is returned if the number is divisable by 3 and 5", () => {
+    // Arrange
+    // Act
+    // Assert
+    expect(prod_code(15)).toBe("FizzBuzz");
   });
 });
